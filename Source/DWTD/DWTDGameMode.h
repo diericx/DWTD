51b38b49aed285f48eb2b9f52247d37945b6d63e
Zac Holland <zacharyholland@gmail.com>
2017-11-08 23:29:30 -0700

    Initial commit

---
+++ Source/DWTD/DWTDGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DWTDGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADWTDGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADWTDGameMode();
+};
+
+
+
